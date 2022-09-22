49e751a2e2f2b275925c1588383ea40736553cc5
kaialex22 <kalexinjp@gmail.com>
2022-09-22 13:44:51 +0900

    vite-template-init

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,8 @@
-import { Engine, Loader } from "excalibur";
-import { Player } from "./player";
-import { Resources } from "./resources";
+import { Engine } from "excalibur";
 
-class Game extends Engine {
-    constructor() {
-      super({width: 800, height: 600});
-    }
-    initialize() {
-      
-      const player = new Player();
-      this.add(player);
+const game = new Engine({
+  width: 800,
+  height: 600,
+});
 
-      const loader = new Loader([Resources.Sword]);
-      this.start(loader);
-    }
-  }
-  
-  export const game = new Game();
-  game.initialize();
+game.start();
